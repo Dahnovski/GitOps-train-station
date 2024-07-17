47a5144e448787432db7f4614d9ca2cf383d7f18
Nazar Dahnovski <nazar.gavrilov20@gmail.com>
2024-07-17 14:39:41 +0300

    Array functionality without testing

--- pract1.c
+++ pract1.c
@@ -1,6 +1,7 @@
 #include 	<stdio.h>
 
 #define		EXIT		0
+#define		SIZE		10
 
 int main(void)
 {
@@ -9,5 +10,9 @@ int main(void)
 
 	printf("Address: %p | Value by address: %d", ptr_a, *ptr_a);
 	
+	int arr[SIZE] = {22, 33, 44, 55};
+	print("Size in bytes of my array: %d\n", sizeof(arr));
+	print("Length of my array: %d", sizeof(arr) / sizeof(*arr));
+
 	return EXIT;
 }
