6e2cf94bf882a6d03ff2e6cd31f9fdcb0ef513b1
Nazar Dahnovski <nazar.gavrilov20@gmail.com>
2024-07-18 17:16:40 +0300

    Final version of C codebase

--- pract1.c
+++ pract1.c
@@ -13,9 +13,17 @@ int main(void)
 	int arr[SIZE] = {22, 33, 44, 55};
 	
 	printf("Size in bytes of my array: %lu\n", sizeof(arr));
-	printf("Length of my array: %lu", sizeof(arr) / sizeof(*arr));
+	printf("Length of my array: %lu\n", sizeof(arr) / sizeof(*arr));
 
 	// this comment shows add line for fix_errors branch
+
+	printf("\n");
+
+	// let's see the elements of the array...
+	for(int i = 0; i < SIZE - 1; i++)
+	{
+		printf("Elem: [%d]: [%d]\n", i, arr[i]);
+	}
 
 	return EXIT;
 }
