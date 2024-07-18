6adeba41c9eda25641b1cd38543683d251053988
Nazar Dahnovski <nazar.gavrilov20@gmail.com>
2024-07-18 16:53:34 +0300

    fix_error branch tracked

--- pract1.c
+++ pract1.c
@@ -11,8 +11,11 @@ int main(void)
 	printf("Address: %p | Value by address: %d", ptr_a, *ptr_a);
 	
 	int arr[SIZE] = {22, 33, 44, 55};
+	
 	printf("Size in bytes of my array: %lu\n", sizeof(arr));
 	printf("Length of my array: %lu", sizeof(arr) / sizeof(*arr));
+
+	// this comment shows add line for fix_errors branch
 
 	return EXIT;
 }
